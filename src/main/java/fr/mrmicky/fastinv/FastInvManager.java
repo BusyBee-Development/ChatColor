f1a271110253bbd827393d31441d0245d04d19ff
djtmk-admin <troymkrause@gmail.com>
2026-05-10 18:49:36 -0400

    Introduce `FastInv` API for streamlined inventory management
    - Added `FastInv` class to simplify inventory creation and handling.
    - Implemented `FastInvManager` for efficient event management and registration.
    - Provided customizable open, close, click, and drag handlers for inventories.

---
+++ src/main/java/fr/mrmicky/fastinv/FastInvManager.java
@@ -0,0 +1,101 @@
+package fr.mrmicky.fastinv;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public final class FastInvManager {
+
+    private static final AtomicBoolean REGISTERED = new AtomicBoolean(false);
+    private FastInvManager() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void register(Plugin plugin) {
+        Objects.requireNonNull(plugin, "plugin");
+
+        if (REGISTERED.getAndSet(true)) {
+            throw new IllegalStateException("FastInv is already registered");
+        }
+
+        Bukkit.getPluginManager().registerEvents(new InventoryListener(plugin), plugin);
+    }
+
+    public static final class InventoryListener implements Listener {
+
+        private final Plugin plugin;
+
+        public InventoryListener(Plugin plugin) {
+            this.plugin = plugin;
+        }
+
+        @EventHandler
+        public void onInventoryClick(InventoryClickEvent e) {
+            if (e.getInventory().getHolder() instanceof FastInv && e.getClickedInventory() != null) {
+                FastInv inv = (FastInv) e.getInventory().getHolder();
+
+                boolean wasCancelled = e.isCancelled();
+                e.setCancelled(true);
+
+                inv.handleClick(e);
+
+                if (!wasCancelled && !e.isCancelled()) {
+                    e.setCancelled(false);
+                }
+            }
+        }
+
+        @EventHandler
+        public void onInventoryDrag(InventoryDragEvent e) {
+            if (e.getInventory().getHolder() instanceof FastInv) {
+                FastInv inv = (FastInv) e.getInventory().getHolder();
+
+                boolean wasCancelled = e.isCancelled();
+                e.setCancelled(true);
+
+                inv.handleDrag(e);
+
+                if (!wasCancelled && !e.isCancelled()) {
+                    e.setCancelled(false);
+                }
+            }
+        }
+
+        @EventHandler
+        public void onInventoryOpen(InventoryOpenEvent e) {
+            if (e.getInventory().getHolder() instanceof FastInv) {
+                FastInv inv = (FastInv) e.getInventory().getHolder();
+
+                inv.handleOpen(e);
+            }
+        }
+
+        @EventHandler
+        public void onInventoryClose(InventoryCloseEvent e) {
+            if (e.getInventory().getHolder() instanceof FastInv) {
+                FastInv inv = (FastInv) e.getInventory().getHolder();
+
+                if (inv.handleClose(e)) {
+                    Bukkit.getScheduler().runTask(this.plugin, () -> inv.open((Player) e.getPlayer()));
+                }
+            }
+        }
+
+        @EventHandler
+        public void onPluginDisable(PluginDisableEvent e) {
+            if (e.getPlugin() == this.plugin) {
+                REGISTERED.set(false);
+            }
+        }
+    }
+}
