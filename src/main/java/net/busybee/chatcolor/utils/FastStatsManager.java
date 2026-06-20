73528a39feb2e658158343c3011435ea6292c68a
djtmk-admin <troymkrause@gmail.com>
2026-06-19 20:49:47 -0400

    Refactor `FastStatsManager` to use `BukkitContext`, update FastStats dependency to `0.27.0`, and bump version to `2026.6.2`.

--- src/main/java/net/busybee/chatcolor/utils/FastStatsManager.java
+++ src/main/java/net/busybee/chatcolor/utils/FastStatsManager.java
@@ -1,14 +1,14 @@
 package net.busybee.chatcolor.utils;
 
-import dev.faststats.bukkit.BukkitMetrics;
-import dev.faststats.core.ErrorTracker;
-import dev.faststats.core.data.Metric;
+import dev.faststats.bukkit.BukkitContext;
+import dev.faststats.ErrorTracker;
+import dev.faststats.data.Metric;
 import net.busybee.chatcolor.ChatColor;
 
 public class FastStatsManager {
     private static final String FAST_STATS_TOKEN = "30d8d244e27052da0508ee242ee389ff";
     private final ChatColor plugin;
-    private final BukkitMetrics metrics;
+    private final BukkitContext context;
 
     public static final ErrorTracker ERROR_TRACKER = ErrorTracker.contextAware()
             .anonymize("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", "[uuid hidden]")
@@ -17,21 +17,22 @@ public class FastStatsManager {
     public FastStatsManager(ChatColor plugin) {
         this.plugin = plugin;
 
-        this.metrics = BukkitMetrics.factory()
-                .token(FAST_STATS_TOKEN)
-                .errorTracker(ERROR_TRACKER)
-                .addMetric(Metric.number("total_colors", () -> (double) plugin.getColorManager().getColorList().size()))
-                .addMetric(Metric.number("total_gradients", () -> (double) plugin.getColorManager().getGradientList().size()))
-                .addMetric(Metric.number("total_patterns", () -> (double) plugin.getPatternManager().getPatternList().size()))
-                .create(plugin);
+        this.context = new BukkitContext.Factory(plugin, FAST_STATS_TOKEN)
+                .errorTrackerService(ERROR_TRACKER)
+                .metrics(factory -> factory
+                        .addMetric(Metric.number("total_colors", () -> (double) plugin.getColorManager().getColorList().size()))
+                        .addMetric(Metric.number("total_gradients", () -> (double) plugin.getColorManager().getGradientList().size()))
+                        .addMetric(Metric.number("total_patterns", () -> (double) plugin.getPatternManager().getPatternList().size()))
+                        .create())
+                .create();
     }
 
     public void onEnable() {
-        metrics.ready();
+        context.ready();
         plugin.getLogger().info("FastStats metrics have been enabled!");
     }
 
     public void onDisable() {
-        metrics.shutdown();
+        context.shutdown();
     }
 }
